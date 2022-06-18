90fb86076509e151fbe5ae86cc11f59b20b17b9e
jose luis ceron imbachi <joseluisceron13@gmail.com>
2022-06-17 20:49:05 -0500

    [feat] aborting a launch api supporting data persistance

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,7 +2,6 @@ const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
-const launches = new Map();
 
 const launch = {
   flightNumber: 100,
@@ -17,8 +16,10 @@ const launch = {
 
 saveLaunch(launch)
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId
+  })
 }
 
 async function getLatestFlightNumber() {
@@ -67,11 +68,19 @@ async function scheduleNewLaunch(launch){
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId
+  }, {
+    upcoming: false,
+    success: false,
+  });
+
+  return aborted.modifiedCount === 1;
+  // const aborted = launches.get(launchId);
+  // aborted.upcoming = false;
+  // aborted.success = false;
+  // return aborted;
 }
 
 module.exports = {
