d1d8faeba4559337b53b80b6f4f624bdfe7d498f
jose luis ceron imbachi <joseluisceron13@gmail.com>
2022-06-21 11:38:57 -0500

    [fix] jest test error

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const {
+const { 
   getAllLaunches,
   scheduleNewLaunch,
   existsLaunchWithId,
@@ -16,21 +16,18 @@ async function httpGetAllLaunches(req, res) {
 }
 
 async function httpAddNewLaunch(req, res) {
-  let launch = req.body;
+  const launch = req.body;
 
-  if (
-    !launch.mission ||
-    !launch.rocket ||
-    !launch.launchDate ||
-    !launch.target
-  ) {
-    return res.status(400).json({
-      error: 'Missing required launch property',
-    });
-  }
+  if (!launch.mission || !launch.rocket || !launch.launchDate
+    || !launch.target) {
+      return res.status(400).json({
+        error: 'Missing required launch property',
+      });
+    }
+
   launch.launchDate = new Date(launch.launchDate);
   if (isNaN(launch.launchDate)) {
-    res.status(400).json({
+    return res.status(400).json({
       error: 'Invalid launch date',
     });
   }
@@ -40,10 +37,9 @@ async function httpAddNewLaunch(req, res) {
 }
 
 async function httpAbortLaunch(req, res) {
-  const launchId = +req.params.id;
+  const launchId = Number(req.params.id);
 
   const existsLaunch = await existsLaunchWithId(launchId);
-
   if (!existsLaunch) {
     return res.status(404).json({
       error: 'Launch not found',
@@ -53,11 +49,12 @@ async function httpAbortLaunch(req, res) {
   const aborted = await abortLaunchById(launchId);
   if (!aborted) {
     return res.status(400).json({
-      error: 'Launch not aborted'
-    })
+      error: 'Launch not aborted',
+    });
   }
+
   return res.status(200).json({
-    ok: true
+    ok: true,
   });
 }
 
